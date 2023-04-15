331416d6dde0a4dbfc6630182d2ba091992865e4
Bruno Reis <bruno.p.reis@gmail.com>
2023-04-14 23:29:23 -0300

    auth model

---
+++ prisma/migrations/20230415020559_start/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "bio" TEXT,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Membership" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "roleId" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+
+    CONSTRAINT "Membership_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,32 +2,154 @@ import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const userData: Prisma.UserCreateInput[] = [
+const permissionData: Prisma.PermissionCreateInput[] = [
+  { id: 'Log In' },
+  { id: 'Log Out' },
+  { id: 'View Lesson List' },
+  { id: 'View Lesson' },
+]
+
+const roleData: Prisma.RoleCreateInput[] = [
+  {id: 'anonymous', permissions:{
+    connect: { id: 'Log In'}
+  }},
+  {id: 'admin', permissions:{
+    connect: [
+      { id: 'View Lesson List'}, 
+      { id: 'View Lesson'},
+      { id: 'Log Out'}
+    ]
+  }},
+  {id: 'teacher', permissions:{
+    connect: [
+      { id: 'View Lesson List'}, 
+      { id: 'View Lesson'},
+      { id: 'Log Out'}
+    ]
+  }},
+  {id: 'student', permissions:{
+    connect: [
+      { id: 'View Lesson List'}, 
+      { id: 'View Lesson'},
+      { id: 'Log Out'}
+    ]
+  }},
+]
+
+const groupData: Prisma.GroupCreateInput[] = [
   {
-    email: "admin@example.com",
-    id: "34075a0d-0431-424b-add0-7c893c6fe0b5",
-    name: "Admin",
-    bio: "Hello, I'm Admin!",
-    role: "admin",
-    password: "$2b$10$KBPVsOCqVBJBCEEzI2S9n.2exIlJoQUX4l6KLjk5pSy5TbZdmo6.O",
+    id: 'group_a',
+    name: 'Group A',
   },
   {
-    email: "teacher@example.com",
-    id: "cfd7d883-93a6-4f15-b7a8-cba0ffc52363",
-    password: "$2b$10$CqlCrFqv5KlEeg926QRAaOAft2t/dINTreuFkn1irQnQl7W.WClNq",
-    name: "Teacher",
-    bio: "Hi, I'm Jane!",
-    role: "teacher",
+    id: 'group_b',
+    name: 'Group B',
   },
 ];
 
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    id: "34075a0d-0431-424b-add0-7c893c6fe0b5",
+    email: "admin@example.com",
+    name: "Admin",
+    bio: "Hello, I'm Admin!",
+    password: "$2b$10$KBPVsOCqVBJBCEEzI2S9n.2exIlJoQUX4l6KLjk5pSy5TbZdmo6.O",
+  },
+  {
+    id: "cfd7d883-93a6-4f15-b7a8-cba0ffc52363",
+    email: "teacher@example.com",
+    password: "$2b$10$CqlCrFqv5KlEeg926QRAaOAft2t/dINTreuFkn1irQnQl7W.WClNq",
+    name: "Teacher",
+    bio: "Hi, I'm Jane!",
+  },
+  {
+    id: "c1f875d9-1889-42f3-8c3b-5f5aa35d1a5f",
+    email: "student1@example.com",
+    password: "$2b$10$CqlCrFqv5KlEeg926QRAaOAft2t/dINTreuFkn1irQnQl7W.WClNq",
+    name: "Student 1",
+    bio: "Hi, I'm John!",
+  },
+  {
+    id: "a918c10a-6d89-4f67-b814-14b86c9e60d2",
+    email: "student2@example.com",
+    password: "$2b$10$CqlCrFqv5KlEeg926QRAaOAft2t/dINTreuFkn1irQnQl7W.WClNq",
+    name: "Student 2",
+    bio: "Hi, I'm Alex!",
+  },
+];
+
+
+const membershipData: Prisma.MembershipCreateInput[] = [
+  {
+    id: "8c2dc6f2-1885-4473-aa5f-1e92ebebbcf5",
+    user: { connect: { email: "admin@example.com" } },
+    group: { connect: { id: "group_a" } },
+    role: { connect: { id: "admin" } },
+  },
+  {
+    id: "c18fdaab-86e6-4de6-ba51-62e96cf9451b",
+    user: { connect: { email: "teacher@example.com" } },
+    group: { connect: { id: "group_a" } },
+    role: { connect: { id: "teacher" } },
+  },
+  {
+    id: "a94e518a-1a80-4c6e-9bdf-0868b1bb6e39",
+    user: { connect: { email: "teacher@example.com" } },
+    group: { connect: { id: "group_b" } },
+    role: { connect: { id: "teacher" } },
+  },
+  {
+    id: "3e89c406-6955-491a-82ee-10b5f5428e17",
+    user: { connect: { email: "student1@example.com" } },
+    group: { connect: { id: "group_b" } },
+    role: { connect: { id: "student" } },
+  },
+  {
+    id: "b5759dfe-68de-4be9-b34d-ff2f67d3e82b",
+    user: { connect: { email: "student2@example.com" } },
+    group: { connect: { id: "group_b" } },
+    role: { connect: { id: "student" } },
+  },
+];
+
+
 async function main() {
   console.log(`Start seeding ...`);
+  // PERMISSIONS
+  for (const p of permissionData) {
+    const permission = await prisma.permission.create({
+      data: p,
+    });
+    console.log(`Created permission with id: ${p.id}`);
+  }
+  // ROLES
+  for (const r of roleData) {
+    const role = await prisma.role.create({
+      data: r,
+    });
+    console.log(`Created role with id: ${r.id}`);
+  }
+  // GROUPS
+  for (const g of groupData) {
+    const group = await prisma.group.create({
+      data: g,
+    });
+    console.log(`Created group with id: ${g.id}`);
+  }
+  // USERS
   for (const u of userData) {
     const user = await prisma.user.create({
       data: u,
     });
     console.log(`Created user with id: ${user.id}`);
+  }
+  // MEMBERSHIPS
+  for (const m of membershipData) {
+    const membership = await prisma.membership.create({
+      data: m,
+    });
+    console.log(`Created membership with id: ${m.id}`);
   }
   console.log(`Seeding finished.`);
 }
