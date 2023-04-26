1b47b4e4a9290dde8ea73eb5e1f88aa2504d6393
Bruno Reis <bruno.p.reis@gmail.com>
2023-04-26 10:22:52 -0300

    implement getRequestFields to inspect graphql queried fields and use them to eager load the prisma relations

--- api/graphql/queries/user.ts
+++ api/graphql/queries/user.ts
@@ -1,10 +1,39 @@
 import { ContextType } from '../../ContextType';
 import { extendType, nonNull, stringArg } from 'nexus'
-import { parseResolveInfo } from 'graphql-parse-resolve-info'
 
 
-const relation = (parsedNode,rootName,relationName) => {
-  return parsedNode?.fieldsByTypeName[rootName][relationName]
+const getRequestedFields = (selections: readonly any[], parentFieldName = ''): string[] => {
+  const requestedFields: string[] = []
+  for (const selection of selections) {
+    if (selection.kind === 'Field') {
+      const fieldName = parentFieldName ? `${parentFieldName}.${selection.name.value}` : selection.name.value
+      requestedFields.push(fieldName)
+      if (selection.selectionSet) {
+        requestedFields.push(...getRequestedFields(selection.selectionSet.selections, fieldName))
+      }
+    } else if (selection.kind === 'FragmentSpread') {
+      // Handle fragment spreads if necessary
+    } else if (selection.kind === 'InlineFragment') {
+      // Handle inline fragments if necessary
+    }
+  }
+  return requestedFields
+}
+
+type IncludeFields = { memberships?: boolean | { include: { group: boolean } } }
+const getIncludeFields = (requestedFields: string[]):IncludeFields  => {
+  const includeFields: { memberships?: boolean | { include: { group: boolean } } } = {}
+  if (requestedFields.includes('memberships')) {
+    includeFields.memberships = true
+    if (requestedFields.includes('memberships.group')) {
+      includeFields.memberships = {
+        include: {
+          group: true,
+        },
+      }
+    }
+  }
+  return includeFields
 }
 
 export const UserQuery = extendType({
@@ -17,30 +46,19 @@ export const UserQuery = extendType({
       },
       authorize: (_root, args, ctx:ContextType) => ctx.auth.loggedIn(),
       resolve(_root, args, ctx, resolveInfo) {
-        const parsedResolveInfoFragment = parseResolveInfo(resolveInfo);
-        let memberships:boolean | object = false;
-
-        const membershipsRelation = relation(parsedResolveInfoFragment,'User', 'memberships');
-        if(membershipsRelation) {
-          memberships = true;
-          const groupRelation = relation(membershipsRelation,'Membership','group');
-          if(groupRelation) {
-            memberships = {
-              include: {
-                group: true
-              }
-            }
-          }
+        let requestedFields: string[] = []
+        if(resolveInfo.fieldNodes[0].selectionSet) {
+          requestedFields = getRequestedFields(resolveInfo.fieldNodes[0].selectionSet.selections)
         }
+        const includeFields = getIncludeFields(requestedFields)
         const params =  {
           include: {
-            memberships
+            ...includeFields
           },
           where: {
             uuid: args.uuid
           }
         }
-
         return ctx.db.user.findUnique(params)
       }
     })
--- api/graphql/queries/users.ts
+++ api/graphql/queries/users.ts
@@ -2,14 +2,32 @@ import { ContextType } from '../../ContextType';
 import { extendType } from 'nexus'
 import { parseResolveInfo } from 'graphql-parse-resolve-info'
 
+const getRequestedFields = (selections: readonly any[]): string[] => {
+  const requestedFields: string[] = []
+  for (const selection of selections) {
+    console.log(selections)
+    if (selection.kind === 'Field') {
+      requestedFields.push(selection.name.value)
+    } else if (selection.kind === 'FragmentSpread') {
+      // Handle fragment spreads if necessary
+    } else if (selection.kind === 'InlineFragment') {
+      // Handle inline fragments if necessary
+    } else if (selection.kind === 'SelectionSet') {
+      requestedFields.push(...getRequestedFields(selection.selections))
+    }
+  }
+  return requestedFields
+}
+
 export const UsersQuery = extendType({
   type: 'Query',
   definition(t) {
     t.nonNull.list.field('users', {
       type: 'User',
       authorize: (_root, _args, ctx:ContextType) => ctx.auth.loggedIn(),
-      resolve(_root, _args, ctx, resolveInfo) {
-        const parsedResolveInfoFragment = parseResolveInfo(resolveInfo);
+      resolve(_root, _args, ctx, resolverInfo) {
+
+        const parsedResolveInfoFragment = parseResolveInfo(resolverInfo);
         let memberships:boolean | object = false;
         const membershipsRelation = parsedResolveInfoFragment?.fieldsByTypeName.User?.memberships;
         if(membershipsRelation) {
