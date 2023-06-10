98be56f8546f527ef7aacf87faeb8c77a632a4c9
Bruno Reis <bruno.p.reis@gmail.com>
2023-06-10 12:33:08 -0300

    local dev db / login unexpected error handling

--- api/graphql/mutations/login/login.ts
+++ api/graphql/mutations/login/login.ts
@@ -14,30 +14,38 @@ export const LoginMutation = extendType({
         password: nonNull(stringArg()),                  
       },
       async resolve(_root, args, ctx:ContextType, resolverInfo:GraphQLResolveInfo) {
-        const query = { 
-          where: { email: args.email }, 
-          include: {
-            memberships: { 
-              include: { 
-                role: { select: { uuid: true }},
-                group: true
+        try {
+          const query = { 
+            where: { email: args.email }, 
+            include: {
+              memberships: { 
+                include: { 
+                  role: { select: { uuid: true }},
+                  group: true
+                }
               }
             }
           }
+          const user = await ctx.prisma.user.findUnique(query);
+          if (!user) {
+            return { errorMessage: "Invalid Email or password" };
+          }
+          const isValidPassword = await bcrypt.compare(args.password, user.password);
+          if (!isValidPassword) {
+            throw new Error("Invalid email or Password");
+          }
+          if(!process.env.JWT_SECRET_KEY) {
+            ctx.logger.error("Please provide the JWT_SECRET_KEY")
+          }
+          const jwt = sign({ userUuid: user.uuid }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "1h",
+          });
+          ctx.currentUser.set(user.uuid)
+          return { jwt, screen: { user } };
+        } catch(e) {
+          ctx.logger.error(e)
+          return { errorMessage: "An unexpected error has occurred. Please try again later." }
         }
-        const user = await ctx.prisma.user.findUnique(query);
-        if (!user) {
-          return { errorMessage: "Invalid Email or password" };
-        }
-        const isValidPassword = await bcrypt.compare(args.password, user.password);
-        if (!isValidPassword) {
-          throw new Error("Invalid email or Password");
-        }
-        const jwt = sign({ userUuid: user.uuid }, process.env.JWT_SECRET_KEY, {
-          expiresIn: "1h",
-        });
-        ctx.currentUser.set(user.uuid)
-        return { jwt, screen: { user } };
       }
     })
   },
